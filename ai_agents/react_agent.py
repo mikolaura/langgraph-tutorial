d3d7af0b74d09c32e867f68cbb6880d5457d0dcc
mikolaura <mikolaura58@gmail.com>
2025-06-19 11:26:47 +0300

    done with react agent and drafter

--- ai_agents/react_agent.py
+++ ai_agents/react_agent.py
@@ -11,3 +11,68 @@ from langgraph.prebuilt import ToolNode
 
 load_dotenv()
 
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+@tool
+def add(a:int, b:int):
+    """This is an addition function that adds 2 numbers together"""
+
+    return a + b
+@tool
+def subtract(a: int, b: int):
+    """This is an subtraction function that subtract 2 numbers"""
+    return a -b
+@tool
+def multiply(a: int, b:int):
+    """This is a multiply function that multiply 2 numbers"""
+    return a * b
+tools = [add, subtract, multiply]
+
+model = ChatGoogleGenerativeAI(model = "gemini-2.0-flash").bind_tools(tools)
+
+def model_call(state:AgentState) -> AgentState:
+    system_prompt = SystemMessage(content=
+        "You are my AI assistant, please answer my query to the best of your ability."
+    )
+    response = model.invoke([system_prompt]+ state["messages"])
+    return {'messages': [response]}
+
+def should_continue(state: AgentState):
+    messages = state['messages']
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else: 
+        return "continue"
+    
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END
+    }
+)
+graph.add_edge("tools", "our_agent")
+
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s['messages'][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {"messages": [('user', "Add 32 + 23. Subtract 2 - 3. Multiply 5*25")]}
+print_stream(app.stream(inputs, stream_mode="values"))
